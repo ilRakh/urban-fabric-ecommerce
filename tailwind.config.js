9fd7638416e5f8be9b970a9e732e183f87e2d730
Lautaro Perez <lautaroperez135@gmail.com>
2023-09-27 20:04:15 -0300

    Revert "pull"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    colors: {
-      "uf-black": "#121212",
-      "uf-background": "#151515",
-      "uf-white": "#ffffff",
-      "uf-gray": "#A7A7A7",
-      "uf-light-gray": "#EFEFEF",
-      "uf-dark-gray": "#3A3A3A",
-      "uf-orange": "#FF4C00",
-      "uf-red": "#E51010",
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
-    extend: {},
   },
   plugins: [],
-};
+}
